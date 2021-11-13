cb46a76a65e9ca604a036638b88f46c4180c22d0
janValasek <janvalasek@hotmail.sk>
2021-11-13 13:57:50 +0100

    Initial commit

---
+++ pearsonCorrection.py
@@ -0,0 +1,41 @@
+from typing import Union, Any
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from numpy import ndarray
+from pandas import Series, DataFrame
+from pandas.core.arrays import ExtensionArray
+from pandas.core.generic import NDFrame
+from pylab import rcParams
+import seaborn as sb
+import scipy
+from scipy.stats import pearsonr
+
+
+rcParams['figure.figsize'] = 5, 4
+sb.set_style('whitegrid')
+
+addresses = r'C:\Users\jvalasek001\Desktop\PyCode\Statistics\Data\mtcars.csv'
+cars = pd.read_csv(addresses)
+cars.columns = ['names', 'mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear', 'carb']
+
+mpg: Union[Union[Series, ExtensionArray, None, ndarray, DataFrame, NDFrame], Any] = cars['mpg']
+hp = cars['hp']
+qsec = cars['qsec']
+vs = cars['vs']
+
+pCor, pValue = pearsonr(mpg, hp)
+print(round(pCor, 3))   
+print(min(mpg))
+
+dfX = cars[['mpg', 'hp', 'qsec', 'vs']]
+corr = dfX.corr()
+print(corr)
+
+
+
+
+
+
+
